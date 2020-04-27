3db5dfcdd10d67eb6189293fc8c9075e0ef168ad
Hippie Hacker <hh@ii.coop>
2020-04-27 22:07:27 +1200

    Update usage so the elpa-mirror for installed packages is created

--- layers/ii-elpa/packages.el
+++ layers/ii-elpa/packages.el
@@ -1,64 +1,23 @@
 ;;; packages.el --- ii-elpa layer packages file for Spacemacs.
 ;;
-;; Copyright (c) 2012-2018 Sylvain Benner & Contributors
+;; Copyright (c) 2020 ii.coop
 ;;
-;; Author: Hippie Hacker <hh@Hippies-MacBook-Pro.local>
-;; URL: https://github.com/syl20bnr/spacemacs
+;; Author: Hippie Hacker <hh@ii.coop>
+;; URL: https://github.com/humacs/.spacemacs.d
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
 ;;; License: GPLv3
 
-;;; Commentary:
-
-;; See the Spacemacs documentation and FAQs for instructions on how to implement
-;; a new layer:
-;;
-;;   SPC h SPC layers RET
-;;
-;;
-;; Briefly, each package to be installed or configured by this layer should be
-;; added to `ii-elpa-packages'. Then, for each package PACKAGE:
-;;
-;; - If PACKAGE is not referenced by any other Spacemacs layer, define a
-;;   function `ii-elpa/init-PACKAGE' to load and initialize the package.
-
-;; - Otherwise, PACKAGE is already referenced by another Spacemacs layer, so
-;;   define the functions `ii-elpa/pre-init-PACKAGE' and/or
-;;   `ii-elpa/post-init-PACKAGE' to customize the package as it is loaded.
-
-;;; Code:
-
 (defconst ii-elpa-packages
   '(
     elpa-mirror
     )
-  "The list of Lisp packages required by the ii-elpa layer.
-
-Each entry is either:
-
-1. A symbol, which is interpreted as a package to be installed, or
-
-2. A list of the form (PACKAGE KEYS...), where PACKAGE is the
-    name of the package to be installed or loaded, and KEYS are
-    any number of keyword-value-pairs.
-
-    The following keys are accepted:
-
-    - :excluded (t or nil): Prevent the package from being loaded
-      if value is non-nil
-
-    - :location: Specify a custom installation location.
-      The following values are legal:
-
-      - The symbol `elpa' (default) means PACKAGE will be
-        installed using the Emacs package manager.
-
-      - The symbol `local' directs Spacemacs to load the file at
-        `./local/PACKAGE/PACKAGE.el'
-
-      - A list beginning with the symbol `recipe' is a melpa
-        recipe.  See: https://github.com/milkypostman/melpa#recipe-format")
-
+  )
+(defun ii-elpa/init-elpa-mirror ()
+  "This package helps create an elpa mirror based on what we install"
+  (use-package elpa-mirror
+    :ensure t)
+    )
 
 ;;; packages.el ends here
