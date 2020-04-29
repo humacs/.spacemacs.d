6314b18fda9269ec7012af62fc55f86daf444993
zach mandeville <zz@ii.coop>
2020-04-29 00:40:41 +0000

    in org-mode, don't ask to execute src blocks each time

--- init.el
+++ init.el
@@ -486,6 +486,13 @@ This function is called at the very end of Spacemacs startup, after layer
 configuration.
 Put your configuration code here, except for variables that should be set
 before packages are loaded."
+;;;;
+;; Org Mode
+;;;;
+
+  ;; don't ask for confirmation every time you execute a code block.
+  (setq org-confirm-babel-evaluate nil)
+
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
