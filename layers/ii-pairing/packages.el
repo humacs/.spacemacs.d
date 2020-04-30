7cb6925301e03ce655742f35643f11927d376a78
Hippie Hacker <hh@ii.coop>
2020-05-01 05:48:20 +1200

    Fully comment out lockstep

--- layers/ii-pairing/packages.el
+++ layers/ii-pairing/packages.el
@@ -1,12 +1,13 @@
 (defconst ii-pairing-packages
   `(
-   (lockstep
-   :ensure t
-   :location ,(concat
-               (configuration-layer/get-layer-local-dir 'ii-pairing)
-               "lockstep")
-   :commands (lockstep lockstep-popup)
-   )
+   ;; Currently not working, but would be nice to make it work
+   ;; (lockstep
+   ;; :ensure t
+   ;; :location ,(concat
+   ;;             (configuration-layer/get-layer-local-dir 'ii-pairing)
+   ;;             "lockstep")
+   ;; :commands (lockstep lockstep-popup)
+   ;; )
   ))
 ;; This appears to no longer work, but the idea looks interesting
 ;; (defun ii-pairing/init-lockstep ()
