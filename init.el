0d5b899b0682f43677a5c6c865501716c17d6db9
zach mandeville <zz@ii.coop>
2020-05-03 22:34:43 +0000

    add s, the long lost string manipulation library, to add'l packages

--- init.el
+++ init.el
@@ -71,7 +71,9 @@ This function should only modify configuration layer settings."
    ;; To use a local version of a package, use the `:location' property:
    ;; '(your-package :location "~/path/to/your-package/")
    ;; Also include the dependencies as they will not be resolved automatically.
-   dotspacemacs-additional-packages '()
+   dotspacemacs-additional-packages '(
+                                      s ; useful elisp string functions
+                                      )
 
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
