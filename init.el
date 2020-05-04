45945e8eae9b1c25397be24c8f035bbdbc4d794e
zach mandeville <zz@ii.coop>
2020-05-04 04:40:58 +0000

    remove commented out layers i don't plan to use

--- init.el
+++ init.el
@@ -57,9 +57,7 @@ This function should only modify configuration layer settings."
      (shell :variables
             shell-default-height 30
             shell-default-position 'bottom)
-     ;; spell-checking
      syntax-checking
-     ;; version-control
      treemacs
      ii-elpa
      ii-org-capture
